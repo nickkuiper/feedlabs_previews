5035ba4d9b262f82e44ebeb2b9f66b0c626979b2
nickkuiper <nickkuiper11@gmail.com>
2022-08-05 15:39:48 +0200

    feeds added

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ feeds = {
     'Borzo Verticals': {'url': 'https://storage.googleapis.com/fl-output-feeds/62Md9zW/BEzpYrm', 'fields' : ['id', 'title'], 'image_field' : ['image[0].url'], 'items' : 3, 'tid' : base_telegram_id},
     'Henk Kuiper Autos': {'url': 'https://storage.googleapis.com/fl-output-feeds/e2MWarL/Q2gqNMw', 'fields' : ['merk', 'model', 'price'], 'image_field' : ['image[0].url'], 'items' : 3, 'tid' : '-446561835'},
     'SE - SV - BL': {'url': 'https://storage.googleapis.com/fl-output-feeds/4LMAaMl/l0zXLz2', 'fields' : ['vehicle_id', 'price'], 'image_field' : ['image[0].url'], 'items' : 2, 'tid' : base_telegram_id},
+    'NO - NB - BL': {'url': 'https://storage.googleapis.com/fl-output-feeds/4LMAaMl/LJgZkM9', 'fields' : ['vehicle_id', 'price'], 'image_field' : ['image[0].url'], 'items' : 2, 'tid' : base_telegram_id},
+    'PO - PO - BL': {'url': 'https://storage.googleapis.com/fl-output-feeds/4LMAaMl/xNrJQzR', 'fields' : ['vehicle_id', 'price'], 'image_field' : ['image[0].url'], 'items' : 2, 'tid' : base_telegram_id},
+    'Ekar': {'url': 'https://storage.googleapis.com/fl-output-feeds/dNMjKzk/BPzOBMQ', 'fields' : ['vehicle_id', 'price'], 'image_field' : ['image[0].url'], 'items' : 2, 'tid' : base_telegram_id},
     #'Henk Kuiper Autos': {'url': 'https://storage.googleapis.com/fl-output-feeds/e2MWarL/Q2gqNMw', 'fields' : ['merk', 'model', 'price'], 'image_field' : ['image[0].url'], 'items' : 3, 'tid' : '-446561835'},
 }
 
